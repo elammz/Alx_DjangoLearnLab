f03072a2fb2ebad38e741128065bb9b802d45a54
elammz <elammizanu4@gmail.com>
2024-08-18 21:59:58 +0300

    modification

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -26,3 +26,36 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from .models import UserProfile  # Make sure UserProfile is imported
+
+# Helper function to check if a user is an Admin
+def is_admin(user):
+    return UserProfile.objects.filter(user=user, role='Admin').exists()
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+# Helper function to check if a user is a Member
+def is_member(user):
+    return UserProfile.objects.filter(user=user, role='Member').exists()
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
+
+# Helper function to check if a user is a Librarian
+def is_librarian(user):
+    return UserProfile.objects.filter(user=user, role='Librarian').exists()
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
